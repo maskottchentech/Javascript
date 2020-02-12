ffe2ce1bd03dbcccb94f4f7dd300449ce287c09a
maskottchentech <54236108+maskottchentech@users.noreply.github.com>
2020-02-12 22:19:09 +0530

    Add files via upload

---
+++ JS button/test.js
@@ -0,0 +1,18 @@
+function myfunction() {
+	var x;
+	var a = 'green';
+	var b = 'red';
+    	document.getElementById("change").style.background = b;
+    
+}
+myfunction();
+
+function myfunction1() {
+	
+	var a = 'green';
+	
+    	document.getElementById("change").style.background = a;
+    
+    
+}
+myfunction1();
